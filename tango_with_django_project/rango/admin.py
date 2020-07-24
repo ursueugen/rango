bfb8342e0132d6189e6f0ac0a324b029c73426e0
Ursu Eugen <ursu_eugen@hotmail.com>
2020-07-24 12:46:52 +0300

    user authentification

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -8,6 +8,7 @@ class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
 # Register your models here.
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -95,6 +95,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -127,3 +128,5 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = "/media/"
+
+LOGIN_URL = '/rango/login/'
